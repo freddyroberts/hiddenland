2835548eed592bfdc000e3ba527231f05257b239
Freddy Roberts <froberts@lewiscommunication.com>
2018-11-13 23:21:01 -0600

    cleanup styles

--- source/components/card/index.js
+++ source/components/card/index.js
@@ -5,34 +5,18 @@ import { RouteHandler } from 'wee-routes';
 export default new RouteHandler({
     init() {
         $events.on({
-            body: {
-                click(e) {
-                    const elements = [
-                        ...$socialContainer.toArray(),
-                        ...$socialContainer
-                            .filter('.js-populated')
-                            .find('*')
-                            .toArray(),
-                        ...$(':socialButton').toArray(),
-                    ];
-
-                    if (! elements.includes(e.target)) {
-                        $('.js-popup').hide();
-                    }
-                }
-            },
             '.js-card': {
                 click(e, el) {
                     e.preventDefault();
-                    $(`.js-popup-${$(el).data('card')}`).show();
-
-                    
+                    $(`.js-popup-${$(el).data('card')}`).show();  
+                    $('body').addClass('-popup');      
                 }
             },
             '.js-close': {
                 click(e, el) {
                     e.preventDefault();
                     $('.popup').hide();
+                    $('body').removeClass('-popup');  
                 }
             }
         });
